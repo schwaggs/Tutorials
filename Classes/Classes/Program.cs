2dfc6a2f82bcb71a2c0de2cf15b8ca89236e0df2
jjschweigert <jschweigert@oemsolutions.net>
2018-09-27 15:12:03 -0400

    Added classes and types/relations

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Husky meshka = new Husky();
+            meshka.Speak();
+            Console.ReadLine();
+        }
+    }
+
+    public class Mammal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("Base Class: Mammal");
+        }
+    }
+
+    public class Dog : Mammal
+    {
+        public override void Speak()
+        {
+            base.Speak();
+            Bark();
+        }
+
+        public virtual void Bark()
+        {
+            Console.WriteLine("Derived Class/Base Class: Dog");
+        }
+    }
+
+    public class Husky : Dog
+    {
+        public override void Bark()
+        {
+            base.Bark();
+            Yip();
+        }
+
+        public void Yip()
+        {
+            Console.WriteLine("Derived Class: Husky");
+        }
+    }
+}
---
+++ TypesAndRelations/TypesAndRelations/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndRelations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Can use relational operators to compare multiple types including the types themselfs
+
+            // See if two static integers are equal, should output "true" because 1 equals 1
+            Console.WriteLine(1 == 1);
+
+            // See if two static strings are equal, should output "false" because "str" is not the same string as "sta"
+            Console.WriteLine("str" == "sta");
+
+            // This should also output "false" because the string "str" is all lowercase while the strign "STR" is all uppercase, and things are case sensitive.
+            Console.WriteLine("str" == "STR");
+
+            // The above comparisons used the equivalency operator ==
+            // Let's use the not equivalent operator != in the case of the comparison above with "str" and "STR"
+
+            // This will return "true" because "str" does not equal "STR"
+            Console.WriteLine("str" != "STR");
+
+            // Next let's look at less than and greater than
+
+            // This will return "false" because 1 is not less than itself
+            Console.WriteLine(1 < 1);
+
+            // This will return true because 0.5 is less than 1
+            Console.WriteLine(0.5 < 1 );
+
+            // Next let's look at less than or equal and greater than or equal
+
+            // This will return true because even though 1 is not less than 1, it is still equal to itself
+            Console.WriteLine(1 <= 1);
+
+            // And the same case for greater than
+            Console.WriteLine(1 >= 1);
+
+            // Now let's look at a few more operators is and as
+
+            // is will return true if the type of something is the type of another
+            char a = 'a';
+            Console.WriteLine(a is int);
+
+            // As will return an object of one type converted to another type
+            object tst = new test();
+
+            test testObj = tst as test;
+
+            Console.WriteLine(testObj.GetType());
+
+            Console.ReadLine();
+        }
+    }
+
+    public class test { }
+}
